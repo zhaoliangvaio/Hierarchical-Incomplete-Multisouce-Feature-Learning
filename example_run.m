3039d3fad1e0eec6e25f8c4f325e63ce806c9bbc
Liang Zhao <zhaoliangvaio@gmail.com>
2016-06-19 14:31:19 -0400

    Revert "upload"
    This reverts commit c1e66ca4d6dc602ab6c0f141fb1349ea98072fc6.

--- example_run.m
+++
@@ -1,8 +0,0 @@
-function example_run
-load published_data;
-[inputs_tr,sizes,XUZ_std] = training_data_formulate(tr_X,tr_U,tr_Z,tr_Y,tr_chunks,5); % the weights of positive obseravations are 5 times than those of negative ones
-[Ws] = interactive_lasso(inputs_tr,sizes,0.5,0.5,0,1);% choose classification using logisitc loss
-inputs_te = test_data_formulate(te_X,te_U,te_Z,te_Y,te_chunks,XUZ_std);
-% load tmp_intermediate_data;
-predict(Ws,inputs_te,1,false);
-end
